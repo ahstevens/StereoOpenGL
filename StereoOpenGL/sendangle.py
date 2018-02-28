24898d5c6eeda77b9e5b5ed9e98b88a1267abd5c
ahstevens <ahstevens@gmail.com>
2018-02-28 00:02:28 -0500

    python tcp client update

--- StereoOpenGL/sendangle.py
+++ StereoOpenGL/sendangle.py
@@ -1,23 +1,32 @@
 import sys
 import socket
 
-if len(sys.argv) < 4:
-    print("Usage:", sys.argv[0], "<host-ip>", "<host-port>", "<servo-angle>")
+if len(sys.argv) < 3:
+    print("Usage:", sys.argv[0], "<host-ip>", "<host-port>", "[servo-angle]")
     sys.exit()
 
 TCP_IP = sys.argv[1]
 TCP_PORT = int(sys.argv[2])
-MSG = str(sys.argv[3])
 BUF_SIZE = 16
 
 print("Host IP:", TCP_IP)
 print("Host port:", TCP_PORT)
-print("Message:", MSG)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
-sock.send(MSG.encode())
-data = sock.recv(BUF_SIZE)
-sock.close
 
-print("Received data:", data.decode())
+if len(sys.argv) == 4:
+    sock.send(sys.argv[3].encode())
+    print("Message:", sys.argv[3])
+
+    data = sock.recv(BUF_SIZE)
+    print("Response:", data.decode())
+else:
+    angle = input("Angle: ")
+    while int(angle) >= 0:
+        sock.send(angle.encode())
+        data = sock.recv(BUF_SIZE)
+        print("Response:", data.decode())
+        angle = input("Angle: ")
+
+sock.close
